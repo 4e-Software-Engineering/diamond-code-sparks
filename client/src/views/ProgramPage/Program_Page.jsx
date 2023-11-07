52039cd165df82cbb5d2fd741091264b246dc8b0
jean-luc <jchoiseul@ufl.edu>
2023-11-07 09:46:04 -0500

    re-added My Programs header

--- client/src/views/ProgramPage/Program_Page.jsx
+++ client/src/views/ProgramPage/Program_Page.jsx
@@ -4,6 +4,11 @@ import './Program_Page.less';
 function ProgramPage() {
   return (
     <div className="my-programs">
+      <div className="header">
+        <button className="back-button"></button>
+        <h1>My Programs</h1>
+        <div className="menu"></div>
+      </div>
       
       <div className="programs-list">
         <div className="program new-program">
